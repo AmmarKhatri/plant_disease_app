d4f01f9127e4ce2ea3d0d9c8504c434a9bb42e54
Ammar Khatri <43322268+AmmarKhatri@users.noreply.github.com>
2023-05-09 20:19:50 +0500

    final version of application

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -12,36 +12,24 @@ class Dashboard extends StatelessWidget {
         body: Container(
           decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("assets/background-image.jpg"),
+            image: AssetImage("assets/plantbackground.jpeg"),
             fit: BoxFit.cover,
           )),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               const SizedBox(
-                height: 40,
-              ),
-              const Card(
-                elevation: 20,
-                color: Color.fromARGB(29, 36, 218, 36),
-                child: Padding(
-                  padding:  EdgeInsets.fromLTRB(90,10,90,10),
-                  child: Image(
-                    height: 150,
-                    width: 170,
-                    image: AssetImage('assets/leaves-in-hand.png')
-                  ),
-                ),
+                height: 250,
               ),
               Card(
                 elevation: 20,
-                color: Color.fromARGB(82, 141, 90, 90),
+                color: const Color.fromARGB(82, 141, 90, 90),
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(8,0,8,0),
                   child: Text("Plant Disease Detection", style: GoogleFonts.poppins(
                     fontSize: 28,
                     fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 227, 236, 218)
+                    color: const Color.fromARGB(255, 227, 236, 218)
                   ),),
                 ),
               ),
@@ -50,13 +38,13 @@ class Dashboard extends StatelessWidget {
                 padding: const EdgeInsets.all(15.0),
                 child: Card(
                   elevation: 20,
-                  color: Color.fromARGB(41, 56, 207, 56),
+                  color: Color.fromARGB(96, 56, 207, 56),
                   child: Padding(
                     padding: const EdgeInsets.all(40.0),
                     child: Text("Provide us with an image and we will let you know your plant's health", style: GoogleFonts.poppins(
                       fontSize:16,
                       fontWeight: FontWeight.w600,
-                      color: Color.fromARGB(255, 235, 223, 219)
+                      color: const Color.fromARGB(255, 235, 223, 219)
                     ),
                     textAlign: TextAlign.center,
                     ),
@@ -65,9 +53,9 @@ class Dashboard extends StatelessWidget {
               ),
               TextButton.icon(
                 onPressed: (){
-                  Get.to(() => Selection());
+                  Get.to(() => const Selection());
                 }, 
-                icon: Icon(Icons.image), 
+                icon: const Icon(Icons.image), 
                 label: Text("Get Started!", style: GoogleFonts.poppins(
                   fontSize: 20,
                   fontWeight: FontWeight.w500
@@ -75,7 +63,7 @@ class Dashboard extends StatelessWidget {
                 style: TextButton.styleFrom(
                   elevation: 20,
                   foregroundColor: const Color.fromARGB(255, 240, 231, 227),
-                  backgroundColor: const Color.fromARGB(176, 184, 86, 48)
+                  backgroundColor: const Color.fromARGB(200, 211, 137, 108)
                 ),
               ),
               const SizedBox(
